fb3314c25e60d2c89caa960c6701e0c2e382b0b2
KaunivaJulia <151840241+KaunivaJulia@users.noreply.github.com>
2023-12-17 02:45:06 +0500

    Add files via upload

---
+++ AppDbcontext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SecondLab
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Interval> Intervals { get; set; }
+
+        public string DbPath { get; }
+
+        public AppDbContext(string dbPath)
+        {
+            DbPath = dbPath;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
---
+++ Interval.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLab
+{
+    [PrimaryKey(nameof(from), nameof(to))]
+    public class Interval
+    {
+        
+        public DateTime from { get; set; }
+        public DateTime to { get; set; }
+
+        public Interval()
+        {
+
+        }
+
+        public int FindInterval()
+        {
+            return (to - from).Days;
+        }
+    }
+}
